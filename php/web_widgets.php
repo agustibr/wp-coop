45da9687402adf06d9b56eeee693e96e394d43c5
Agusti Monty <agusti@bibtic.com>
2011-11-16 02:12:23 +0100

    millores en funcions de sidebars i menus, i desfer config del tema pare

--- php/web_widgets.php
+++ php/web_widgets.php
@@ -14,40 +14,29 @@ function widget_num_setmana($args) {
 }
 register_sidebar_widget(_('Num. de Setmana'), 'widget_num_setmana');
 
+// esborrem configuracions del tema pare (toolbox)
+function remove_toolbox_setup(){
+	//unregister_sidebar( 'sidebar-1' );
+	unregister_sidebar( 'sidebar-2' );
+	remove_theme_support( 'post-formats');
+	remove_theme_support( 'automatic-feed-links'); 
+}
+add_action( 'init', 'remove_toolbox_setup', 11);
 
-function toolbox_setup() {
-	load_theme_textdomain( 'toolbox', get_template_directory() . '/languages' );
+// creem configuracions del tema (menus, sidebars...)
+function coop_setup() {
 
-	$locale = get_locale();
-	$locale_file = get_template_directory() . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
-
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
-
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
 	register_nav_menus( array(
-		'primary' => __( 'Menu Public', 'wp-coop' ),
-		'ruscaire' => __( 'Menu Ruscaires', 'wp-coop' ),
-		'proveidor_arc' => __( 'Menu Arc Natura', 'wp-coop' ),
+		'primary' => __( 'Menú Public', 'wp-coop' ),
+		'ruscaire' => __( 'Menú Ruscaires', 'wp-coop' ),
+		'proveidor_arc' => __( 'Menú Arc Natura', 'wp-coop' ),
 	) );
 
-	/**
-	 * Add support for the Aside and Gallery Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'gallery' ) );//'aside', 'image', 
-}
-add_action( 'after_setup_theme', 'toolbox_setup' );
 
-function toolbox_widgets_init() {
 	register_sidebar( array(
-		'name' => __( 'Sidebar 1', 'toolbox' ),
+		'name' => __( 'Sidebar 1', 'wp-coop' ),
 		'id' => 'sidebar-1',
+		'description' => __( 'Lateral sempre present', 'wp-coop' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => "</aside>",
 		'before_title' => '<h1 class="widget-title">',
@@ -59,9 +48,9 @@ function toolbox_widgets_init() {
 	foreach ( $wp_roles->role_names as $role => $name ) :
 		if($role!='administrator'):
 			register_sidebar( array(
-				'name' => __( 'Sidebar ' . $name , 'toolbox' ),
+				'name' => __( 'Sidebar ' . $name , 'wp-coop' ),
 				'id' => 'sidebar-'  . $role,
-				'description' => __( 'Lateral pel rol ' . $name, 'toolbox' ),
+				'description' => __( 'Lateral pel rol ' . $name, 'wp-coop' ),
 				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 				'after_widget' => "</aside>",
 				'before_title' => '<h1 class="widget-title">',
@@ -71,14 +60,14 @@ function toolbox_widgets_init() {
 	endforeach;
 	
 	register_sidebar( array(
-		'name' => __( 'Sidebar No Logejats', 'toolbox' ),
+		'name' => __( 'Sidebar No Logejats', 'wp-coop' ),
 		'id' => 'sidebar-no_logged',
-		'description' => __( 'Lateral pels No Logejats', 'toolbox' ),
+		'description' => __( 'Lateral pels No Logejats', 'wp-coop' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => "</aside>",
 		'before_title' => '<h1 class="widget-title">',
 		'after_title' => '</h1>',
 	) );
-}
-add_action( 'init', 'toolbox_widgets_init' );
 
+}
+add_action( 'after_setup_theme', 'coop_setup' );
