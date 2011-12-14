a00e6ebe98c4d8869d48fa0ecb72db6e378391e5
Agusti Monty <agusti@bibtic.com>
2011-12-14 01:20:16 +0100

    Styles for Protected Page Form CSS : estils form pg Protegida Amb Contrasenya IMG : icon-lock.png , apareix al titol de pg protegida PHP : afegit filter custom_password_form()

--- functions.php
+++ functions.php
@@ -92,21 +92,50 @@ function extensions() {
 
 }
 extensions();
+
 // Remove Private and Protected Prefix. This function removes the "Privite:" prefix from posts and pages marked private.
 function the_title_trim($title) {
 $title = attribute_escape($title);
 $findthese = array(
-    '#Protected:#',
+    '#Protegit:#',
     '#Privat:#'
 );
 $replacewith = array(
-    '', // What to replace "Protected:" with
+    '<span class="icon-lock"></span>', // What to replace "Protected:" with
     '' // What to replace "Private:" with
 );
 $title = preg_replace($findthese, $replacewith, $title);
 return $title;
 }
 add_filter('the_title', 'the_title_trim');
+
+/**
+ * Retrieve protected post password form content.
+ */
+add_filter( 'the_password_form', 'custom_password_form' );
+function custom_password_form() {
+    global $post;
+    $label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
+    $output = '
+    <form class="protected-post-form" action="' . get_option('siteurl') . '/wp-pass.php" method="post">
+        <fieldset>
+            <legend>' . __( "Aquesta entrada està protegida.", "wp-coop" ) . '</legend>
+            <div class="clearfix">
+                <label for="' . $label . '"><br/><br/>' . __( "Password:" ) . ' </label>
+                <div class="input">
+                    <span class="help-block">' . __( "Per veure-la, introduïu la vostra contrasenya:", "wp-coop" ) . '</span>
+                    <input name="post_password" id="' . $label . '" type="password" size="20" />
+                </div>
+                <div class="actions">
+                    <input type="submit" name="Submit" value="' . esc_attr__( "Submit" ) . '" class="btn primary"/>
+                </div>
+            </div>
+        </fieldset> 
+    </form>
+    ';
+    return $output;
+}
+
 /*
 // WP PERFONMANCE
 // Display DB Queries, Time Spent and Memory Consumption
