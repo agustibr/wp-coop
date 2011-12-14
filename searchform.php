ad0f3ef25cca083582af251122328e991191d2d1
Agusti Monty <agusti@bibtic.com>
2011-12-14 02:09:50 +0100

    Styles for Sidebar Search Form
    N : template searchform.php
    CSS : estils search form
    IMG : icon-search.png , apareix al submit button

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
+	<fieldset class="clearfix">
+		<input type="text" value="" name="s" id="s" class="span3" placeholder="<?php _e('Cerca', 'wp-coop'); ?> <?php bloginfo('name'); ?>"/>
+		<button type="submit" id="searchsubmit" class="btn info">&nbsp;<span><?php _e('Cerca', 'wp-coop'); ?></span></button>
+	</fieldset>
+</form> 
